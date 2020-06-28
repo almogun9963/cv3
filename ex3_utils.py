2485ad7b99610426b7348495682bb62097343748
almogun9963 <47495987+almogun9963@users.noreply.github.com>
2020-06-28 17:32:51 +0300

    Add files via upload

--- ex3_utils.py
+++ ex3_utils.py
@@ -7,14 +7,6 @@ import matplotlib.pyplot as plt
 
 
 def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10, win_size=5) -> (np.ndarray, np.ndarray):
-    """
-    Given two images, returns the Translation from im1 to im2
-    :param im1: Image 1
-    :param im2: Image 2
-    :param step_size: The image sample size:
-    :param win_size: The optical flow window size (odd number)
-    :return: Original points [[y,x]...], [[dU,dV]...] for each points
-    """
     if win_size % 2 == 0:
         win_size = win_size - 1
 
@@ -50,12 +42,6 @@ def getkernel() -> np.ndarray:
 
 
 def laplaceianReduce(img: np.ndarray, levels: int = 4) -> List[np.ndarray]:
-    """
-    Creates a Laplacian pyramid
-    :param img: Original image
-    :param levels: Pyramid depth
-    :return: Laplacian Pyramid (list of images)
-    """
     im = [img]
     kernel = getkernel()
     laplaceian = []
@@ -68,11 +54,6 @@ def laplaceianReduce(img: np.ndarray, levels: int = 4) -> List[np.ndarray]:
 
 
 def laplaceianExpand(lap_pyr: List[np.ndarray]) -> np.ndarray:
-    """
-    Resotrs the original image from a laplacian pyramid
-    :param lap_pyr: Laplacian Pyramid
-    :return: Original image
-    """
     gaussian = [lap_pyr[-1]]
     kernel = getkernel()
     laplaceian = lap_pyr.copy()
@@ -83,12 +64,6 @@ def laplaceianExpand(lap_pyr: List[np.ndarray]) -> np.ndarray:
 
 
 def gaussianPyr(img: np.ndarray, levels: int = 4) -> List[np.ndarray]:
-    """
-    Creates a Gaussian Pyramid
-    :param img: Original image
-    :param levels: Pyramid depth
-    :return: Gaussian pyramid (list of images)
-    """
     kernel = getkernel()
     gaussianPyr = [img]
     for i in range(1, levels):
@@ -98,12 +73,6 @@ def gaussianPyr(img: np.ndarray, levels: int = 4) -> List[np.ndarray]:
 
 
 def gaussExpand(img: np.ndarray, gs_k: np.ndarray) -> np.ndarray:
-    """
-    Expands a Gaussian pyramid level one step up
-    :param img: Pyramid image at a certain level
-    :param gs_k: The kernel to use in expanding
-    :return: The expanded level
-    """
     if (len(img.shape) == 2):
         weight = img.shape[0]
         hight = img.shape[1]
@@ -123,14 +92,6 @@ def gaussExpand(img: np.ndarray, gs_k: np.ndarray) -> np.ndarray:
 
 
 def pyrBlend(img_1: np.ndarray, img_2: np.ndarray, mask: np.ndarray, levels: int) -> (np.ndarray, np.ndarray):
-    """
-    Blends two images using PyramidBlend method
-    :param img_1: Image 1
-    :param img_2: Image 2
-    :param mask: Blend mask
-    :param levels: Pyramid depth
-    :return: Blended Image
-    """
     blend1 = img_1 * mask + img_2 * (1 - mask)
     a = laplaceianReduce(img_1, levels)
     b = laplaceianReduce(img_2, levels)
